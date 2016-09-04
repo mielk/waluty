bc21e6ae7b85c1b211c9a2b2c05a70dc817163c9
mielk <mielk@o2.pl>
2016-09-04 03:54:45 +0200

    Dodany plik z testami jednostkowymi dla klasy TrendlineAnalyzer.

---
+++ Stock-UnitTest/Stock.Domain/Services/Analyzers/TrendlineAnalyzerUnitTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Stock_UnitTest.Stock.Domain.Services.Analyzers
+{
+    [TestClass]
+    public class TrendlineAnalyzerUnitTests
+    {
+        //Testy sprawdzające czy odpowiednie metody są wywoływane dla odpowiednich DataItemów.
+
+    }
+}
